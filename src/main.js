a5b8475eb4782359afd7491af9bc1ae5a661d60b
Ann <shokh.anna@mail.ru>
2023-01-17 22:27:53 +0300

    Add presenter

--- src/main.js
+++ src/main.js
@@ -1,18 +1,22 @@
 import TripInfoView from './view/trip-info-view.js';
 import FilterView from './view/filter-view.js';
-import SortView from './view/sort-view.js';
+import MainPresenter from './presenter/main-presenter.js';
+
 import {render, RenderPosition} from './render.js';
 
 const siteHeaderElement = document.querySelector('.page-header');
 const headerInfoContainer = siteHeaderElement.querySelector('.trip-main');
-const filterContainer = headerInfoContainer.querySelector('.trip-controls__filters');
+const filterContainer = siteHeaderElement.querySelector('.trip-controls__filters');
 
 const siteMainElement = document.querySelector('.page-main');
 const eventsContainer = siteMainElement.querySelector('.trip-events');
 
-
+const mainPresenter = new MainPresenter();
 
 render(new TripInfoView(), headerInfoContainer, RenderPosition.AFTERBEGIN);
 render(new FilterView(), filterContainer);
 
-render(new SortView(), eventsContainer);
+mainPresenter.init(eventsContainer);
+
+
+
