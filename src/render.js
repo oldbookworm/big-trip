ec1b20b92ba798ac894b6395f5f98848bd825b85
Ann <shokh.anna@mail.ru>
2023-01-30 22:33:55 +0300

    code refactoring

--- src/render.js
+++ src/render.js
@@ -13,7 +13,8 @@ const createElement = (template) => {
 };
 
 const render = (component, container, place = RenderPosition.BEFOREEND) => {
-  container.insertAdjacentElement(place, component.getElement());
+  container.insertAdjacentElement(place, component.element);
 };
+
 
 export {RenderPosition, createElement, render};
