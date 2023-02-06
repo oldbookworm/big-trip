962c89fc712d4852e9dfa2e5fa550fdb012d3118
Ann <shokh.anna@mail.ru>
2023-02-06 18:49:01 +0300

    Presenter ref

--- src/presenter/main-presenter.js
+++ src/presenter/main-presenter.js
@@ -1,12 +1,11 @@
 import TripInfoView from '../view/trip-info-view.js';
 import SortView from '../view/sort-view.js';
-import FormEditView from '../view/form-view/form-edit-view.js';
-import FormAddNewView from '../view/form-view/form-addnew-view.js';
 import EventsListView from '../view/trip-events-view/events-list-view.js'
-import EventItemView from '../view/trip-events-view/event-item-view.js';
 import EmptyPageView from '../view/empty-page-view.js';
 import NewEventBtnView from '../view/new-event-btn-view.js';
-import {render,  RenderPosition, replace, remove} from '../framework/render.js';
+import PointPresenter from './point-presenter.js';
+import NewFormPresenter from './new-form-presenter.js';
+import {render,  RenderPosition} from '../framework/render.js';
 
 
 export default class MainPresenter {
@@ -20,7 +19,6 @@ export default class MainPresenter {
 	#emptyPageComponent = new EmptyPageView();
 	#tripInfoComponent = new TripInfoView();
 	#newEventBtnComponent = new NewEventBtnView();
-	#newFormComponent = new FormAddNewView();
 	
 	constructor(container, headerInfoContainer, pointsModel) {
 		this.#container = container;
@@ -31,94 +29,55 @@ export default class MainPresenter {
 	init = () => {	
 		this.#points = [...this.#pointsModel.points];
 		this.#renderPointBoard();
-
 	}
 	
 	#renderPointBoard = () => {
-		render(this.#newEventBtnComponent, this.#headerInfoContainer);
-    	render(this.#eventsListComponent, this.#container);
+		this.#renderNewEventBtn();
+    	this.#renderEventsList();
 
 		if(this.#points.length === 0) {
-			render(this.#emptyPageComponent, this.#container);
+			this.#renderEmptyPage();
 		} else {
-			render(this.#tripInfoComponent, this.#headerInfoContainer,  RenderPosition.AFTERBEGIN);
-			render(this.#sortComponent, this.#container, RenderPosition.AFTERBEGIN);
-			for (let i = 0; i < this.#points.length; i++) {
-				this.#renderPoint(this.#points[i]);
-			}	
-		}
+			this.#renderTripInfo();
+			this.#renderSort();
+			this.#renderPoints();
+		}		
 		
-		this.#renderAddNewForm();
-  };
+		this.#newEventBtnComponent.setNewBtnClickHandler(() => {
+			const newFormPresenter = new NewFormPresenter(this.#eventsListComponent.element);
+			newFormPresenter.init();
+		});
+   };
 
-  
-  #renderAddNewForm = () => {	
+   #renderNewEventBtn = () => {
+    	render(this.#newEventBtnComponent, this.#headerInfoContainer);
+  	}
+
+	#renderEventsList = () => {
+		render(this.#eventsListComponent, this.#container);
+	}
+	  
+	#renderEmptyPage = () => {
+		render(this.#emptyPageComponent, this.#container);
+	}
+	  
+	#renderTripInfo = () => {
+		render(this.#tripInfoComponent, this.#headerInfoContainer,  RenderPosition.AFTERBEGIN);
+	}
 	
-	this.#newEventBtnComponent.setNewBtnClickHandler(() => {
-		render(this.#newFormComponent,  this.#eventsListComponent.element, RenderPosition.AFTERBEGIN);
-
-		this.#newFormComponent.setRollupBtnClickHandler(() => {
-			closeNewForm();
-		});
-
-		this.#newFormComponent.setCancelBtnClickHandler(() => {
-			closeNewForm();
-		});
-
-		document.addEventListener('keydown', closeNewFormOnEsc);
-	});
-
-	const closeNewForm = () => {
-		remove(this.#newFormComponent);
+	#renderSort = () => {
+		render(this.#sortComponent, this.#container, RenderPosition.AFTERBEGIN);
 	}
-
-	const closeNewFormOnEsc = (evt) => {
-		if (evt.key === 'Escape' || evt.key === 'Esc') {
-			evt.preventDefault();
-			remove(this.#newFormComponent);
-			document.removeEventListener('keydown', closeNewFormOnEsc);
-		}
+	  
+	#renderPoints = () => {
+		for (let i = 0; i < this.#points.length; i++) {
+			this.#renderPoint(this.#points[i]);
+		}	
 	}
-  }
-
 
   #renderPoint = (point) => {
-    const pointComponent = new EventItemView(point);
-	const formEditComponent = new FormEditView(point);
-
-	const replacePointToForm = () => {
-		replace(formEditComponent, pointComponent);
-	};
-  
-	const replaceFormToPoint = () => {
-		replace(pointComponent, formEditComponent);
-	};
-
-	const onEscKeyDown = (evt) => {
-		if (evt.key === 'Escape' || evt.key === 'Esc') {
-		  evt.preventDefault();
-		  replaceFormToPoint();
-		  document.removeEventListener('keydown', onEscKeyDown);
-		}
-	};
-  
-	pointComponent.setEditClickHandler(() => {
-		replacePointToForm();
-		document.addEventListener('keydown', onEscKeyDown);
-	});
-  
-	formEditComponent.setFormSubmitHandler(() => {
-		evt.preventDefault();
-		replaceFormToPoint();
-		document.removeEventListener('keydown', onEscKeyDown);
-	});
-
-	formEditComponent.setFormCloseBtnClickHandler(() => {
-		replaceFormToPoint();
-	   document.removeEventListener('keydown', onEscKeyDown);
-	});
-
-    render(pointComponent, this.#eventsListComponent.element);
+    const pointPresenter = new PointPresenter(this.#eventsListComponent.element);
+   	pointPresenter.init(point);
   };
 
 
