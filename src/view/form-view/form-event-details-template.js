d1b777185e1198b1f4b0aef99d61dac60e2d19cd
Ann <shokh.anna@mail.ru>
2023-02-23 12:49:32 +0300

    add allOffers obj

--- src/view/form-view/form-event-details-template.js
+++ src/view/form-view/form-event-details-template.js
@@ -2,12 +2,12 @@ import {createEventOffersTemplate} from './event-offers-template.js';
 import {createEventDestinationTemplate} from './event-destination-template.js';
 
 export const createEventDetailsTemplate = (point) => {
-  const {destination: {description, pictures}, type, offers} = point;
-
+  const {destination: {description, pictures}, type, allOffers} = point;
+  
 	return (
 	`<section class="event__details">
 
-    ${createEventOffersTemplate(offers, type)}
+    ${createEventOffersTemplate(allOffers, type)}
 
     ${createEventDestinationTemplate(description, pictures)}
   </section>`
