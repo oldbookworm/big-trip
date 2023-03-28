a0bae103d583701c267885a2cf4953d038574bd8
Ann <shokh.anna@mail.ru>
2023-03-28 22:38:18 +0300

    Add handlers

--- src/presenter/new-form-presenter.js
+++ src/presenter/new-form-presenter.js
@@ -1,54 +1,71 @@
 import FormAddNewView from '../view/form-view/form-addnew-view.js';
 import {render,  RenderPosition, replace, remove} from '../framework/render.js';
+import { UserAction, UpdateType } from '../util/main-util.js';
 
 export default class NewFormPresenter {
     #newFormContainer = null;
     #newFormComponent = null;
-    #closeNewFormHandler = null;
-    #closeOnEscHandler = null;
+    #changeData = null;
+    #destroyCallback = null;
     
-    constructor(newFormContainer, closeNewFormHandler, closeOnEscHandler) {
+    constructor(newFormContainer, changeData) {
       this.#newFormContainer = newFormContainer;
-      this.#closeNewFormHandler = closeNewFormHandler;
-      this.#closeOnEscHandler = closeOnEscHandler;
+      this.#changeData = changeData;
     }
     
-    init = () => {
+    init = (callback) => {
+      this.#destroyCallback = callback;
 
-    const prevNewFormComponent = this.#newFormComponent; 
-    this.#newFormComponent = new FormAddNewView(); 
-     
-     this.#newFormComponent.setRollupBtnClickHandler(() => {
-        this.#closeNewFormHandler();
-        document.removeEventListener('keydown', this.#closeOnEscHandler);
-      });
-  
-     this.#newFormComponent.setCancelBtnClickHandler(() => {
-        this.#closeNewFormHandler();
-        document.removeEventListener('keydown', this.#closeOnEscHandler);
-     });
+      if (this.#newFormComponent !== null) {
+        return;
+      }
+
+     this.#newFormComponent = new FormAddNewView();
+     this.#newFormComponent.setSubmitNewPointHandler(this.#handleAddNewPoint);
+     this.#newFormComponent.setCancelBtnClickHandler(this.#handleDeleteClick);
+     this.#newFormComponent.setRollupBtnClickHandler(this.#handleDeleteClick);
       
-    document.addEventListener('keydown', this.#closeOnEscHandler);
-
-    
-    if(prevNewFormComponent === null) {
-      this.#renderNewForm();
-      return;
+     render(this.#newFormComponent,  this.#newFormContainer, RenderPosition.AFTERBEGIN);
+      
+     document.addEventListener('keydown', this.#escKeyDownHandler);
     }
-
-    replace(this.#newFormComponent, prevNewFormComponent);
-
-    remove(prevNewFormComponent);
     
-  }
-    
-
-  #renderNewForm = () => {
-      render(this.#newFormComponent,  this.#newFormContainer, RenderPosition.AFTERBEGIN);        
-  }
-
-  destroy = () => {
+    destroy = () => {
+      if(this.#newFormComponent === null) {
+        return;
+      }
+      
+      this.#destroyCallback?.();
+  
       remove(this.#newFormComponent);
-  };
-    
-  } 
+      
+      this.#newFormComponent = null;
+      
+      document.removeEventListener('keydown', this.#escKeyDownHandler);
+    };
+      
+  
+    #handleAddNewPoint = (point) => {
+      this.#changeData(
+        UserAction.ADD_POINT,
+        UpdateType.MINOR,
+        {...point},
+      );
+      this.destroy();
+    };
+  
+  
+    #handleDeleteClick = () => {
+      this.destroy();
+    };
+  
+  
+    #escKeyDownHandler = (evt) => {
+      if (evt.key === 'Escape' || evt.key === 'Esc') {
+        evt.preventDefault();
+        this.destroy();
+      }
+    };
+
+
+} 
