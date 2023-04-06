582ec6a643ab6c7f50f406e0cf0c8e1fcb7856ec
Ann <shokh.anna@mail.ru>
2023-04-06 23:42:56 +0300

    Add filters

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import FilterView from './view/filter-view.js';
 import PointsModel from './model/points-model.js';
+import FilterModel from './model/filter-model.js';
 import MainPresenter from './presenter/main-presenter.js';
+import FilterPresenter from './presenter/filter-presenter.js';
+import NewEventBtnView from './view/new-event-btn-view.js';
+import PointsApiService from './services/point-api-service.js';
 
 import {render} from './framework/render.js';
+
+const AUTHORIZATION = 'Basic f45lfnso7d2';
+const END_POINT = 'https://17.ecmascript.pages.academy/big-trip/';
 
 const siteHeaderElement = document.querySelector('.page-header');
 const headerInfoContainer = siteHeaderElement.querySelector('.trip-main');
@@ -11,12 +17,35 @@ const filterContainer = siteHeaderElement.querySelector('.trip-controls__filters
 const siteMainElement = document.querySelector('.page-main');
 const eventsContainer = siteMainElement.querySelector('.trip-events');
 
-const pointsModel = new PointsModel();
-const mainPresenter = new MainPresenter(eventsContainer, headerInfoContainer, pointsModel);
+const pointsModel = new PointsModel(new PointsApiService(END_POINT, AUTHORIZATION));
+const filterModel = new FilterModel();
+const mainPresenter = new MainPresenter(eventsContainer, headerInfoContainer, pointsModel, filterModel);
+const filterPresenter = new FilterPresenter(filterContainer, filterModel,  pointsModel);
+const newEventBtnComponent = new NewEventBtnView();
 
-render(new FilterView(), filterContainer);
+const OFFER_BY_TYPE = pointsModel.offersByType;
 
+
+const handleNewPointFormClose = () => {
+    newEventBtnComponent.element.disabled = false;
+};
+  
+const handleNewPointButtonClick = () => {
+    mainPresenter.createTask(handleNewPointFormClose);
+    newEventBtnComponent.element.disabled = true;
+};
+  
+
+
+filterPresenter.init();
 mainPresenter.init();
 
+pointsModel.init()
+  .finally(() => {
+    render(newEventBtnComponent, headerInfoContainer);
+    newEventBtnComponent.setNewBtnClickHandler(handleNewPointButtonClick);
+  });
+
+export {OFFER_BY_TYPE};
 
 
