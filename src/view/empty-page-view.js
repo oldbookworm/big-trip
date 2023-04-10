5e2547b40cf9017c52c98ff6ea4a2e795c0d7179
Ann <shokh.anna@mail.ru>
2023-04-10 22:03:43 +0300

    Add text for empty page

--- src/view/empty-page-view.js
+++ src/view/empty-page-view.js
@@ -1,13 +1,31 @@
 import AbstractView from '../framework/view/abstract-view.js';
+import { FilterType } from '../util/filter-util.js';
 
-const createEmptyPageTemplate = () => (
-  `<p class="trip-events__msg">Click New Event to create your first point</p>`
-  );
+const NoPointsTextType = {
+  [FilterType.EVERYTHING]: 'Click «ADD NEW» in menu to create your first event',
+  [FilterType.FUTURE]: 'There are no future events now',
+  [FilterType.PAST]: 'There are no past events now',
+};
+
+
+const createEmptyPageTemplate = (filterType) => {
+  const noPointsText = NoPointsTextType[filterType];
+
+  return (
+    `<p class="trip-events__msg">${noPointsText}</p>`
+  )
+};
 
 export default class EmptyPageView extends AbstractView {
+  #filterType = null;
+
+  constructor(filterType) {
+    super();
+    this.#filterType = filterType;
+  }
   
   get template() {
-    return createEmptyPageTemplate();
+    return createEmptyPageTemplate(this.#filterType);
   }
 
 }
