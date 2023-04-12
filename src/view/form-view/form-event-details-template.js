995fdf78ec22c2cb83e22690fbfa1b47e27c729e
Ann <shokh.anna@mail.ru>
2023-04-12 21:35:12 +0300

    Add server

--- src/view/form-view/form-event-details-template.js
+++ src/view/form-view/form-event-details-template.js
@@ -1,13 +1,13 @@
 import {createEventOffersTemplate} from './event-offers-template.js';
 import {createEventDestinationTemplate} from './event-destination-template.js';
 
-export const createEventDetailsTemplate = (point) => {
+export const createEventDetailsTemplate = (point, allOffers) => {
   const {destination: {description, pictures}, type, offers} = point;
   
 	return (
 	`<section class="event__details">
 
-    ${createEventOffersTemplate(offers, type)}
+    ${createEventOffersTemplate(offers, type, allOffers)}
 
     ${createEventDestinationTemplate(description, pictures)}
   </section>`
